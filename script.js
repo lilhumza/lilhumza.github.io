81f902c2a46ce836d832a33e5b6379504a67ad6d
Humza Khokhar <lilhumza@gmail.com>
2018-03-18 20:03:36 -0400

    HK Tribute Page

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#btnHinge").click(function(){
+       $('body').addClass('animated hinge')
+     });
+    $("#btnChem").click(function(){
+         alert("The Struggle Is Real")
+      $('body').fadeOut(65)
+     alert("Life Is Like A Canvas... It Begins Blank & Everyday Is Like Another Brush Stroke")
+      });
+    function myFunction() {
+      alert("Don't Play Yourself");
+    }
+  });
